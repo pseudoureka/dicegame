a75949c1eef44364f4dc2ba3f12309a382963c7d
pseudoureka <messiah7179@gmail.com>
2024-10-09 23:11:51 +0900

    중괄호를 활용한 자바스크립트 변수 넣는 방법

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import ReactDOM from "react-dom/client";
 
+const item = "MacBook";
+function handleClick() {
+  alert("곧 도착합니다!");
+}
+const imgURL = "https://upload.wikimedia.org/";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+  <>
+    <h1>나만의 {item} 만들기!</h1>
+    <img src={imgURL} alt="이미지" />
+    <button onClick={handleClick}>확인</button>
+  </>
+);
